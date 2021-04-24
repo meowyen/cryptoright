485ee6f507ae097362fa28c20ab9b0389aaa6325
Kelly Domico <kdomico@gmail.com>
2021-04-24 13:32:04 -0700

    Fixed address

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // Change this address to match your deployed contract!
-const contract_address = "0xa5b70b3f9e02b2d0b85E10042a76b1A7F397Cc6b";
+const contract_address = "0x1E376e3EB9F9253785C1455A11aeE2086af12793";
 
 const dApp = {
   ethEnabled: function() {
